5b22f0c8118f6ef9829ffcaf2b07ab14299a4ea1
David <davidrpy@gmail.com>
2022-05-27 11:54:00 -0500

    arreglo git

--- mi primer proyecto/miprimerApp/MiprimerApp/src/app/siderbar/siderbar.component.ts
+++ mi primer proyecto/miprimerApp/MiprimerApp/src/app/siderbar/siderbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicioDeFavoritosService } from '../servicio-de-favoritos.service';
 
 @Component({
   selector: 'app-siderbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SiderbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private servicioFavoritos: ServicioDeFavoritosService) { }
 
   ngOnInit(): void {
   }
