3ad14dbcc8221b27f2763e91a490124b461b3d3b
David <davidrpy@gmail.com>
2022-07-12 14:08:49 -0500

    solicitudes http

--- second try/shoping-list/src/app/services/item.service.ts
+++ second try/shoping-list/src/app/services/item.service.ts
@@ -5,33 +5,33 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ItemService {
-  items:Item[] = [];
+  items:Item[] = [
+    {
+      id:0,
+      title:"manzana",
+      price:10.5,
+      quantity:4,
+      completed:false
+    },
+    {
+      id:1,
+      title:"pan",
+      price:3.5,
+      quantity:8,
+      completed:true
+    },
+    {
+      id:2,
+      title:"chamarra",
+      price:300,
+      quantity:1,
+      completed:false
+    }
+  ];
   constructor() { }
   getItems()
    {
-    this.items=[
-      {
-        id:0,
-        title:"manzana",
-        price:10.5,
-        quantity:4,
-        completed:false
-      },
-      {
-        id:1,
-        title:"pan",
-        price:3.5,
-        quantity:8,
-        completed:true
-      },
-      {
-        id:2,
-        title:"chamarra",
-        price:300,
-        quantity:1,
-        completed:false
-      }
-    ];
+    // this.items=
     return this.items;
    }
    addItem(item:Item)
